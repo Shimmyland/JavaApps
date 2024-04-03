99f3d6e528e11272e2d465e862a64c001563701a
Simon Libiger <116663110+Shimmyland@users.noreply.github.com>
2024-04-03 11:46:15 +0200

    Delete ProjectMovieRental directory

--- ProjectMovieRental/src/main/java/org/example/projectmovierental/services/MovieService.java
+++
@@ -1,8 +0,0 @@
-package org.example.projectmovierental.services;
-
-import org.example.projectmovierental.models.DTOs.MovieResponseDTO;
-
-public interface MovieService {
-
-    MovieResponseDTO getMovies(String title);
-}
